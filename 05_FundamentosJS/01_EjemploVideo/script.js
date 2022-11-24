9c057f593f26da5014e49b360c40b5df2ef0b0b9
reybritez <stevenreybritez@gmail.com>
2022-11-23 23:08:22 -0300

    clase miercoles 23-nov

---
+++ 05_FundamentosJS/01_EjemploVideo/script.js
@@ -0,0 +1,10 @@
+console.log("page loaded...");
+
+function mensajeAlerta(){
+    alert("Felicidades por dar clic")
+}
+
+function cambiarImagen(elemento){
+    elemento.src = "https://www.adslzone.net/app/uploads-adslzone.net/2019/04/borrar-fondo-imagen.jpg"
+    console.log(elemento)
+}
